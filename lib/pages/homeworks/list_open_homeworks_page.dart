564c0999f8f52deee1f68d2347f3630950a57a23
Reynaldo GT <larz.dev@gmail.com>
2022-12-07 17:26:10 -0400

    fix the ui in the list open homeworks

--- lib/pages/homeworks/list_open_homeworks_page.dart
+++ lib/pages/homeworks/list_open_homeworks_page.dart
@@ -36,82 +36,72 @@ class _ListOpenHomeworksPageState extends State<ListOpenHomeworksPage> {
       appBar: AppBarTitle(
         appBar: AppBar(),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              ChipChoice(
-                elementsList: filter.getListLevelSelected,
-                onClickAction: (String value) {
-                  filter.removeItemFromList(value);
-                  homeworksBloc.getHomeworksByCategory(
-                    filter.getListLevelSelected,
-                  );
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(0),
-                    child: SizedBox(
-                      child: GestureDetector(
-                        onTap: () {
-                          showFilterBottomMenuSheet(context);
-                        },
-                        child: Row(
-                          children: const [
-                            Icon(
-                              Icons.filter_list,
-                              color: Colors.grey,
-                            ),
-                            SimpleText(
-                              text: 'Filtrar busqueda',
-                              lightThemeColor: Colors.grey,
-                            ),
-                          ],
-                        ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            ChipChoice(
+              elementsList: filter.getListLevelSelected,
+              onClickAction: (String value) {
+                filter.removeItemFromList(value);
+                homeworksBloc.getHomeworksByCategory(
+                  filter.getListLevelSelected,
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: SizedBox(
+                    child: GestureDetector(
+                      onTap: () {
+                        showFilterBottomMenuSheet(context);
+                      },
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.filter_list,
+                            color: Colors.grey,
+                          ),
+                          SimpleText(
+                            text: 'Filtrar busqueda',
+                            lightThemeColor: Colors.grey,
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    constraints: BoxConstraints(),
-                    onPressed: () {
-                      showSearch(
-                          context: context, delegate: OpenHomeworkSearch());
-                    },
-                    icon: const Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
+                ),
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(),
+                  onPressed: () {
+                    showSearch(
+                        context: context, delegate: OpenHomeworkSearch());
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
                   ),
-                ],
-              ),
-              StreamBuilder(
+                ),
+              ],
+            ),
+            Expanded(
+              child: StreamBuilder(
                 stream: homeworksBloc.homeworksStream,
                 builder: (BuildContext context,
                     AsyncSnapshot<List<HomeworksModel>> snapshot) {
                   if (!snapshot.hasData) {
-                    return SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height * 0.75,
-                      child: const Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
+                    return const CircularProgressIndicator();
                   }
                   if (snapshot.data!.isEmpty) {
-                    return SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.7,
-                      /* color: Colors.green, */
-                      child: const NoInformation(
-                        message: 'No se encontraron tareas',
-                        icon: Icons.search_off,
-                        showButton: false,
-                        iconButton: Icons.task,
-                      ),
+                    return const NoInformation(
+                      message: 'No se encontraron tareas',
+                      icon: Icons.search_off,
+                      showButton: false,
+                      iconButton: Icons.task,
                     );
                   }
 
@@ -132,8 +122,8 @@ class _ListOpenHomeworksPageState extends State<ListOpenHomeworksPage> {
                   );
                 },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
