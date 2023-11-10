8dcaffbe1c2a26d9c3f6f13a5b9f73d4f36cafcd
Reynaldo GT <larz.dev@gmail.com>
2023-11-10 13:01:25 -0400

    added refresh controller in list open homeworks

--- lib/pages/homeworks/list_open_homeworks_page.dart
+++ lib/pages/homeworks/list_open_homeworks_page.dart
@@ -11,6 +11,7 @@ class _ListOpenHomeworksPageState extends State<ListOpenHomeworksPage> {
   late int defaultChoiceIndex;
   late HomeworksProvider homeworksProvider;
   late AuthProvider authProvider;
+  final _refreshController = RefreshController(initialRefresh: false);
   @override
   void initState() {
     super.initState();
@@ -46,101 +47,108 @@ class _ListOpenHomeworksPageState extends State<ListOpenHomeworksPage> {
       body: Consumer<HomeworksProvider>(
         builder: (_, oneHomeworkProviderC, child) {
           final homeworksConsumer = homeworksProvider.state.homeworks;
-          return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                ChipChoice(
-                  elementsList: filter.getListLevelSelected,
-                  onClickAction: (String value) {
-                    filter.removeItemFromList(value);
-                    homeworksProvider.getHomeworksByCategory(
-                      filter.getListLevelSelected,
-                    );
-                  },
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(0),
-                      child: SizedBox(
-                        child: GestureDetector(
-                          onTap: () {
-                            showFilterBottomMenuSheet(context);
-                          },
-                          child: const Row(
-                            children: [
-                              Icon(
-                                Icons.filter_list,
-                                color: Colors.grey,
-                              ),
-                              SimpleText(
-                                text: 'Filtrar busqueda',
-                                lightThemeColor: Colors.grey,
-                              ),
-                            ],
+          return SmartRefresher(
+            controller: _refreshController,
+            onRefresh: () {
+              homeworksProvider.getHomeworks();
+              _refreshController.refreshCompleted();
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  ChipChoice(
+                    elementsList: filter.getListLevelSelected,
+                    onClickAction: (value) {
+                      filter.removeItemFromList(value);
+                      homeworksProvider.getHomeworksByCategory(
+                        filter.getListLevelSelected,
+                      );
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(0),
+                        child: SizedBox(
+                          child: GestureDetector(
+                            onTap: () {
+                              showFilterBottomMenuSheet(context);
+                            },
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.filter_list,
+                                  color: Colors.grey,
+                                ),
+                                SimpleText(
+                                  text: 'Filtrar busqueda',
+                                  lightThemeColor: Colors.grey,
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                      onPressed: () {
-                        showSearch(
-                            context: context,
-                            delegate: OpenHomeworkSearch(
-                              homeworksProvider: oneHomeworkProviderC,
-                            ));
-                      },
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-                oneHomeworkProviderC.state.isLoadingHomeworks
-                    ? const Expanded(
-                        child: Center(
-                          child: CircularProgressIndicator(),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        onPressed: () {
+                          showSearch(
+                              context: context,
+                              delegate: OpenHomeworkSearch(
+                                homeworksProvider: oneHomeworkProviderC,
+                              ));
+                        },
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.grey,
                         ),
-                      )
-                    : homeworksConsumer.isEmpty
-                        ? const Expanded(
-                            child: NoInformation(
-                              message: 'No se encontraron tareas',
-                              icon: Icons.search_off,
-                              showButton: false,
-                              iconButton: Icons.task,
-                            ),
-                          )
-                        : Expanded(
-                            child: ListView.builder(
-                              physics: const NeverScrollableScrollPhysics(),
-                              scrollDirection: Axis.vertical,
-                              shrinkWrap: true,
-                              itemCount: homeworksConsumer.length,
-                              itemBuilder: (_, int index) {
-                                final homework = homeworksConsumer[index];
-                                return HomeworkCard(
-                                  isLogged: authProvider.isLogged
-                                      ? authProvider.isLogged
-                                      : false,
-                                  homework: homework,
-                                  isOwner:
-                                      homework.user.id == authProvider.user.id,
-                                  onSelected: (HomeworksModel homework) {
-                                    context.push(
-                                      '/homework_detail/${homework.id}',
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                          )
-              ],
+                      ),
+                    ],
+                  ),
+                  oneHomeworkProviderC.state.isLoadingHomeworks
+                      ? const Expanded(
+                          child: Center(
+                            child: CircularProgressIndicator(),
+                          ),
+                        )
+                      : homeworksConsumer.isEmpty
+                          ? const Expanded(
+                              child: NoInformation(
+                                message: 'No se encontraron tareas',
+                                icon: Icons.search_off,
+                                showButton: false,
+                                iconButton: Icons.task,
+                              ),
+                            )
+                          : Expanded(
+                              child: ListView.builder(
+                                physics: const NeverScrollableScrollPhysics(),
+                                scrollDirection: Axis.vertical,
+                                shrinkWrap: true,
+                                itemCount: homeworksConsumer.length,
+                                itemBuilder: (_, int index) {
+                                  final homework = homeworksConsumer[index];
+                                  return HomeworkCard(
+                                    isLogged: authProvider.isLogged
+                                        ? authProvider.isLogged
+                                        : false,
+                                    homework: homework,
+                                    isOwner: homework.user.id ==
+                                        authProvider.user.id,
+                                    onSelected: (HomeworksModel homework) {
+                                      context.push(
+                                        '/homework_detail/${homework.id}',
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                            )
+                ],
+              ),
             ),
           );
         },
